546bd6d5a4f6599eac24694dd28e97357f0f3d91
mihaifirst <varzaru.alexmihai@gmail.com>
2024-05-20 11:46:10 +0300

    exercise-2

--- script.js
+++ script.js
@@ -61,29 +61,92 @@
        grade (Should be 84.33)
 */
 
-class Student {
-  constructor(name, major, grades) {
-    this.name = name;
-    this.major = major;
-    this.grades = grades;
+// class Student {
+//   constructor(name, major, grades) {
+//     this.name = name;
+//     this.major = major;
+//     this.grades = grades;
+//   }
+
+//   addGrade(grade) {
+//     this.grades.push(grade);
+//   }
+
+//   gpa() {
+//     let averageGrade = 0;
+//     for (const grade of this.grades) {
+//       averageGrade += grade;
+//     }
+//     return averageGrade / this.grades.length;
+//   }
+// }
+
+// const eva = new Student("Eva", "Arts", [95, 75, 83]);
+// console.log(eva);
+
+// console.log(eva.gpa());
+// eva.addGrade(89);
+// console.log(eva.gpa());
+
+/*
+    1. Create a class called "Bookstore"
+
+    2. The "Bookstore" constructor has a single parameter
+       called "books" which is an Array of Book Objects.
+       Assign the argument passed in to the instance within
+       the constructor.
+
+    3. Create a "listBooks" method on the "Bookstore" class
+       that will loop through and print out all the Books
+       in the Store (assume they have a name and author)
+       so you can format each like this:
+       `${book.name} by ${book.author}`
+
+    3. Create another class called "Book"
+
+    4. The "Book" constructor has 2 parameters:
+       - name
+       - author
+       Assign these to the instance within the constructor
+
+    5. Create 2 Books:
+       const nineteen84 = new Book("1984", "George Orwell")
+       const hp = new Book("Harry Potter", "J.K. Rowling")
+
+    6. Create a Bookstore:
+       const bookstore = new Bookstore( [ nineteen84, hp ] )
+
+    7. Call bookstore.listBooks() and make sure it works:
+       1984 by George Orwell
+       Harry Potter by J.K. Rowling
+*/
+
+class Bookstore {
+  constructor(books) {
+    this.books = books;
   }
 
-  addGrade(grade) {
-    this.grades.push(grade);
-  }
-
-  gpa() {
-    let averageGrade = 0;
-    for (const grade of this.grades) {
-      averageGrade += grade;
+  listBooks() {
+    for (let book of this.books) {
+      //   console.log(`${book.name} by ${book.author}`);
+      book.displayBook();
     }
-    return averageGrade / this.grades.length;
   }
 }
 
-const eva = new Student("Eva", "Arts", [95, 75, 83]);
-console.log(eva);
+class Book {
+  constructor(name, author) {
+    this.name = name;
+    this.author = author;
+  }
 
-console.log(eva.gpa());
-eva.addGrade(89);
-console.log(eva.gpa());
+  displayBook() {
+    console.log(`${this.name} by ${this.author}`);
+  }
+}
+
+const nineteen84 = new Book("1984", "George Orwell");
+const hp = new Book("Harry Potter", "J.K. Rowling");
+
+const bookstore = new Bookstore([nineteen84, hp]);
+bookstore.listBooks();
